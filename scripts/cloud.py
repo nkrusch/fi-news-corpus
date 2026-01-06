26b618d0cffa2a8dc6a1015a3a272415f0e252e1
Neea <neearusch@gmail.com>
2026-01-06 17:24:32 +0100

    speed improvement

--- scripts/cloud.py
+++ scripts/cloud.py
@@ -22,8 +22,6 @@ plt.rcParams['font.family'] = 'sans-serif'
 PLT_TITLE = 'Sanapilvi: Yleisimmät puheenaiheet'
 
 files = [f for f in os.listdir(CORPUS_PATH) if f.endswith('.csv')]
-col_frt = lambda f: pd.read_csv(join(CORPUS_PATH, f))[COLUMNS].astype(str)
-texts = [x for sl in map(col_frt, files) for x in sl.to_numpy().flatten()]
 
 nltk.download('stopwords')
 nlp = spacy.load('fi_core_news_lg')
@@ -31,11 +29,12 @@ IGNORE = pd.read_csv(EXTRA_STOPWORDS, header=None)
 STOPWORDS = set(stopwords.words('finnish')) | set(IGNORE[0])
 
 counts = Counter()
-for text in tqdm(texts, desc="Processing text"):
-    tokens = filter(lambda t: t.pos_ in KINDS, nlp(text))
-    lemmas = map(lambda t: t.lemma_.lower(), tokens)
-    keep = filter(lambda x: x.isalpha() and x not in STOPWORDS, lemmas)
-    counts += Counter(keep)
+for file in tqdm(files, desc="Processing files"):
+    for doc in nlp.pipe((pd.read_csv(join(CORPUS_PATH, file))[COLUMNS]
+            .astype(str).agg(' '.join, axis=1).tolist()), batch_size=500):
+        for x in [t.lemma_.lower() for t in doc if t.pos_ in KINDS]:
+            if x.isalpha() and x not in STOPWORDS:
+                counts[x] += 1
 
 wc = WordCloud(width=1600, height=800, max_words=200, margin=10,
                background_color="white", colormap="viridis",
